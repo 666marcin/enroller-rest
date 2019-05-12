f13a447d8fbef50e8bd0f8df031c79fc28bdf315
student10 <student10@pcoip.ki.agh.edu.pl>
2019-05-12 11:59:33 +0200

    fda

--- src/test/java/com/company/enroller/controllers/ParticipantRestControllerTest.java
+++ src/test/java/com/company/enroller/controllers/ParticipantRestControllerTest.java
@@ -5,8 +5,10 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import java.util.Collection;
 
@@ -18,10 +20,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.company.enroller.model.Participant;
 import com.company.enroller.persistence.MeetingService;
 import com.company.enroller.persistence.ParticipantService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ParticipantRestController.class)
@@ -48,5 +52,60 @@ public class ParticipantRestControllerTest {
 		mvc.perform(get("/participants").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
 				.andExpect(jsonPath("$", hasSize(1))).andExpect(jsonPath("$[0].login", is(participant.getLogin())));
 	}
+	
+	@Test
+	public void getParticipant() throws Exception {
+		Participant participant = new Participant();
+		participant.setLogin("testlogin");
+		participant.setPassword("testpassword");
 
+		given(participantService.findByLogin("testlogin")).willReturn(participant);
+
+		mvc.perform(get("/participants/testlogin").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().string(new ObjectMapper().writeValueAsString(participant)));
+	}
+
+	@Test
+	public void addParticipant() throws Exception {
+		Participant participant = new Participant();
+		participant.setLogin("testlogin");
+		participant.setPassword("testpassword");
+		
+		String partcipantStr = new ObjectMapper().writeValueAsString(participant);
+
+		mvc
+        .perform(MockMvcRequestBuilders.post("/participants").content(partcipantStr).contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isCreated())
+        .andExpect(content().string(new ObjectMapper().writeValueAsString(participant)));
+	}
+	
+	@Test
+    public void addExistingParticipant() throws Exception {
+        
+        Participant participant = new Participant();
+        participant.setLogin("testlogin");
+        participant.setPassword("testpassword");
+        given(participantService.findByLogin(participant.getLogin())).willReturn(participant);
+
+        String partcipantStr = new ObjectMapper().writeValueAsString(participant);
+        
+        mvc
+        .perform(MockMvcRequestBuilders.post("/participants").content(partcipantStr).contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isConflict());
+    }
+	
+	@Test
+    public void deleteExistingParticipant() throws Exception {
+        
+        Participant participant = new Participant();
+        participant.setLogin("testlogin");
+        participant.setPassword("testpassword");
+        given(participantService.findByLogin(participant.getLogin())).willReturn(participant);
+
+        String partcipantStr = new ObjectMapper().writeValueAsString(participant);
+        
+        mvc
+        .perform(MockMvcRequestBuilders.post("/participants").content(partcipantStr).contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isConflict());
+    }
 }
